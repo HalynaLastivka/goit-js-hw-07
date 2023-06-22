dc5bef54d94fa3e293a21d4a1e04a803578b0316
HalynaLastivka <lastivkagalina@gmail.com>
2023-06-22 20:48:43 +0300

    f

---
+++ src/src/js/01-gallery.js
@@ -0,0 +1,60 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+let instance;
+const ulEl = document.querySelector('.gallery');
+const newElImg = createLi(galleryItems);
+
+ulEl.insertAdjacentHTML('beforeend', newElImg);
+const refItem = document.querySelectorAll('.gallery__link');
+const liEl = document.querySelectorAll('.gallery__image');
+
+
+liEl.forEach(function (liItem) {
+    liItem.addEventListener('click', onImgClick); 
+})
+
+
+// cancel event on ref
+refItem.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+  });
+});
+
+
+function createLi(gallery) {
+
+   return gallery.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`; }).join('');   
+}
+
+function onImgClick(event) {
+    const dataSource = event.currentTarget.getAttribute('data-source');
+
+     instance = basicLightbox.create(
+    `<img src="${dataSource}" width="800" height="600">`)
+
+instance.show()
+
+}
+
+window.addEventListener('keydown', pressEsc);
+
+function pressEsc(event) {
+
+  if (event.key !== "Escape") {
+    return;
+  }
+  instance.close();
+}
---
+++ src/src/js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+// import SimpleLightbox from 'simplelightbox';
+
+
+const ulEl = document.querySelector('.gallery');
+const newElImg = createLi(galleryItems);
+
+ulEl.insertAdjacentHTML('beforeend', newElImg);
+
+// SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a',{ captionDelay: 250,captionPosition: 'bottom',captionsData: 'alt',});
+
+
+function createLi(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
